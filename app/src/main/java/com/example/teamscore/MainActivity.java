8485d887cef4afd956134aa0a2befd960682ab48
hemji1912 <hemantvish1999@gmail.com>
2021-09-11 00:24:32 -0700

    Final Code

--- app/src/main/java/com/example/teamscore/MainActivity.java
+++ app/src/main/java/com/example/teamscore/MainActivity.java
@@ -2,66 +2,116 @@ package com.example.teamscore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+     Boolean a= true,b=false;
 
    int scoreA = 0;
    int scoreB = 0;
+   MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this,R.raw.tone);
     }
 
 
-    private void DisplayA(int number) {
-        TextView teamA = (TextView) findViewById(R.id.scoreA);
-        teamA.setText(" "+number);
+    private void Display(int number) {
+
+        if(a){
+            TextView teamA = (TextView) findViewById(R.id.scoreA);
+            teamA.setText(" "+number);
+
+        }
+        if(b){
+            TextView teamB = (TextView)findViewById(R.id.scoreB);
+            teamB.setText(" " + number);
+        }
+        if(a&&b){
+            TextView teamA = (TextView) findViewById(R.id.scoreA);
+            teamA.setText(" "+number);
+            TextView teamB = (TextView)findViewById(R.id.scoreB);
+            teamB.setText(" " + number);
+            a = true;
+            b = false;
+        }
 
     }
-    private void DisplayB(int number) {
-        TextView teamB = (TextView)findViewById(R.id.scoreB);
-        teamB.setText(" " + number);
-    }
+
     public void plusThreeA(View view)
-    {  scoreA += 3;
-    DisplayA(scoreA);
-
+    {  if(a&&!b) {
+        mediaPlayer.start();
+        scoreA += 3;
+        Display(scoreA);
+        b = true;
+        a = false;
+    }
     }
     public void plusThreeB(View view)
-    {   scoreB += 3;
-        DisplayB(scoreB);
-
+    {   if(!a && b) {
+        scoreB += 3;
+        mediaPlayer.start();
+        Display(scoreB);
+        a = true;
+        b = false;
+    }
     }
     public void plusTwoA(View view)
-    {  scoreA += 2;
-        DisplayA(scoreA);
+    {  if(a&&!b) {
+        mediaPlayer.start();
+        scoreA += 2;
+        Display(scoreA);
+        b = true;
+        a = false;
+    }
     }
     public void plusTwoB(View view)
-    { scoreB += 2;
-        DisplayB(scoreB);
+    { if(!a&&b)
+    {mediaPlayer.start();
+        scoreB += 2;
+        Display(scoreB);
+        a = true;
+        b = false;
+    }
+
     }
     public void freeFallA(View view)
-    {  scoreA += 1;
-        DisplayA(scoreA);
+    {  if(a&&!b) {
+        mediaPlayer.start();
+        scoreA += 1;
+        Display(scoreA);
+        b = true;
+        a = false;
+    }
     }
     public void freeFallB(View view)
-    {  scoreB += 1;
-        DisplayB(scoreB);
+    {  if(!a&&b) {
+        mediaPlayer.start();
+        scoreB += 1;
+
+        Display(scoreB);
+        a = true;
+        b = false;
+    }
     }
 
     public void reset(View view)
     {
+        mediaPlayer.start();
         int num = 0;
         scoreB = 0;
         scoreA = 0;
-        DisplayA(num);
-        DisplayB(num);
+        a = true;
+        b = true;
+        Display(num);
+        Display(num);
     }
 
 
